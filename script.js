4185dd4d227a1d7dd4d3bc1510bb8ad1a408f18d
Tim <39227111+badgerbadgers@users.noreply.github.com>
2021-12-02 15:41:58 -0800

    Add files via upload
    Added styling changes, app now works as intended. On page load app will render a random star wars character pulled from API data. There will be several values displayed as well as a button that indicates new character will be generated. On click of this button new character data will be pulled from API and displayed on screen for user.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* function that gets API data, 
 on call of function it generates a number between 1 and 83 */
-
 async function getData() {
-    /* Generate a random number between 1 and 83 */
-let randomNum = Math.floor(Math.random() * 83) + 1;
+    /* variable that generates a random number between 1 and 83 */
+    let randomNum = Math.floor(Math.random() * 83) + 1;
+    /* variable that contains url to API data with a template literal (${}) as the 
+    index.*/
     const url = `https://swapi.dev/api/people/${randomNum}`;
     /* uses fetch method to get api data then json to  */
     try {
@@ -13,39 +14,38 @@ let randomNum = Math.floor(Math.random() * 83) + 1;
         console.log(error);
     }
 }
-function handleClick(event) {
-    event.preventDefault();
-    alert('hi')
+
+/* a function that will run when 'generate new character' is clicked will render new character */
+function handleClick() {
+    renderUsers();
 }
-/* function that will display API data to the webpage. uses object */
-async function renderUsers() {
+
+/* function that will display API data on webpage, uses
+  template literal to render specifc values from pulled API data
+*/async function renderUsers() {
     let user = await getData()
     console.log(user)
-  
-
     let html = '';
-    let htmlSegment = `
-    <div class="child">
-    <div class="user">
-    <h1>Character Spotlight:<h1>
-                            <h2> ${user.name}</h2>
-                            <h3>Birth year: ${user.birth_year}</h3>
-                            <h3>Hair color: ${user.hair_color}</h3>
-                            <h3>Eye color: ${user.eye_color}</h3>
-                            <h3>Height: ${user.height}cm</h3>
-                            <h3>Gender: ${user.gender}</h3>
-                            <button>Generate New Character</button>
-                        </div>
-                        </div>
-                        `;
-
+    let htmlSegment = 
+        `
+        <div class="child">
+         <div class="user">
+          <h1>Character Spotlight:<h1>
+            <h2> ${user.name}</h2>
+              <h3>Birth year: ${user.birth_year}</h3>
+              <h3>Hair color: ${user.hair_color}</h3>
+              <h3>Eye color: ${user.eye_color}</h3>
+              <h3>Height: ${user.height}cm</h3>
+              <h3>Gender: ${user.gender}</h3>
+            <button onClick="handleClick()">Generate New Character</button>
+         </div>
+        </div>
+        `;
     html += htmlSegment;
     let container = document.querySelector('.container');
     container.innerHTML = html;
     };
 
-  document.addEventListener('click', function () {
-      renderUsers();
-  })
 
+/* call render users function on page load*/
 renderUsers();
